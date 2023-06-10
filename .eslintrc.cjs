b51206c4b45f8faca4ee10ca1c91ae7362f9465c
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-06-10 19:46:34 -0400

    chore: automatizacion eslint + conv. commits con husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'vue',
+    ],
+    rules: {
+        // Add your custom rules here
+    },
+};
