71745f8599d4a0f8fffd894494ca79d5f6a3b1f1
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-06-10 20:24:32 -0400

    chore: automatizacion prettier con husky
    formateará el codigo .js y .vue mal formateado automaticamente al hacer un commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: [
-        'vue',
-    ],
-    rules: {
-        // Add your custom rules here
-    },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "vue",
+  ],
+  rules: {
+    "vue/script-indent": ["error", 2, { "baseIndent": 1 }],
+    // Agrega tus reglas personalizadas aquí
+  },
+  overrides: [
+    {
+      files: ["*.vue"],
+      rules: {
+        "indent": "off"
+      }
+    }
+  ]
 };
