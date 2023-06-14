8e7c99cc20dd1df08a56c526f101d1ae8f28cff2
luckas <luckas.strnadalvarez@gmail.com>
2023-06-14 16:43:11 -0400

    fix: areglo menor y formateo del codigo
    cerre una llave y evite el spameo de logs por parte del Yoke

--- src/main.js
+++ src/main.js
@@ -1,30 +1,42 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
 
-let timeoutThrottle;
+let timeoutThrottle
 
-window.addEventListener("keypress",(event) => {
-    if(event.key === "c"){
-        clearTimeout(timeoutThrottle);
-        store.dispatch("presionarThrottle");
-        console.log("throttle_depth", store.getters.getThrottleDepth)
-    }
-    else if(event.key === "v"){
-        clearTimeout(timeoutThrottle);
-        store.dispatch("tirarThrottle");
-        console.log("throttle_depth", store.getters.getThrottleDepth)
-    }
-
+window.addEventListener('keypress', (event) => {
+  if (event.key === 'c') {
+    clearTimeout(timeoutThrottle)
+    store.dispatch('presionarThrottle')
+    console.log('throttle_depth', store.getters.getThrottleDepth)
+  } else if (event.key === 'v') {
+    clearTimeout(timeoutThrottle)
+    store.dispatch('tirarThrottle')
+    console.log('throttle_depth', store.getters.getThrottleDepth)
+  }
 })
-window.addEventListener('keydown', event => {
+window.addEventListener('keydown', (event) => {
+  if (
+    event.key === 'a' ||
+    event.key === 'w' ||
+    event.key === 's' ||
+    event.key === 'd'
+  ) {
     const key = event.key.toUpperCase()
     store.dispatch('presionarTecla_yoke', key)
-  })
-  
-  // Evento para soltar una tecla
-  window.addEventListener('keyup', () => {
-    store.dispatch('soltarTecla_yoke')
-  })
+  }
+})
 
-createApp(App).use(store).mount('#app');
+// Evento para soltar una tecla
+window.addEventListener('keyup', () => {
+  if (
+    event.key === 'a' ||
+    event.key === 'w' ||
+    event.key === 's' ||
+    event.key === 'd'
+  ) {
+    store.dispatch('soltarTecla_yoke')
+  }
+})
+
+createApp(App).use(store).mount('#app')
