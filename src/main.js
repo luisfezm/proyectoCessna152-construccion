994fd8bc52903f602ae0ee6e37a1fb64a3b6b99a
sctavo <gsgsnchz09@gmail.com>
2023-06-13 23:24:27 -0400

    feat: ruddler peddals
    Controlador componente ruddler peddals

--- src/main.js
+++ src/main.js
@@ -2,5 +2,33 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+let timeoutPedalIzq;
+let timeoutPedalDerecha;
 
+window.addEventListener("keydown", (event) => {
+  if (event.key === "q") {
+    clearTimeout(timeoutPedalIzq);
+    store.dispatch("incrementarMovPedalIzq");
+    console.log("mov_Pedal_izq:", store.getters.movPedalIzq);
+  } else if (event.key === "e") {
+    clearTimeout(timeoutPedalDerecha);
+    store.dispatch("incrementarMovPedalDerecha");
+    console.log("mov_Pedal_derecha:", store.getters.movPedalDerecha);
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  if (event.key === "q") {
+    timeoutPedalIzq = setTimeout(() => {
+      store.dispatch("resetMovPedalIzq");
+      console.log("mov_Pedal_izq reset to 0");
+    }, 1000);
+  } else if (event.key === "e") {
+    timeoutPedalDerecha = setTimeout(() => {
+      store.dispatch("resetMovPedalDerecha");
+      console.log("mov_Pedal_derecha reset to 0");
+    }, 1000);
+  }
+});
+
+createApp(App).use(store).mount('#app');
