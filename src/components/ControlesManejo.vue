db2153994b20a2cbeb5356f947056c3ff5728cd2
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-06-13 22:38:52 -0400

    fix: se visualiza artificial horizon correctamente

--- src/components/ControlesManejo.vue
+++ src/components/ControlesManejo.vue
@@ -4,7 +4,6 @@
       <div class="PrimeraFila">
         <ContenedorCircularEjemplo />
         <ArtificialHorizon />
-        <ContenedorCircularEjemplo />
         <ContenedorCircular />
         <ContenedorCircular />
         <ContenedorCircular />
