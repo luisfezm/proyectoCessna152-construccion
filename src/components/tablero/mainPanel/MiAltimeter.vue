3760478adeb250404662e435484e915d37f58948
Diego Mancilla <dmancilla19@alumnos.utalca.cl>
2023-06-14 00:31:12 -0400

    feat: agujas altimeter
    se implento las agujas del altimetro

--- src/components/tablero/mainPanel/MiAltimeter.vue
+++ src/components/tablero/mainPanel/MiAltimeter.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="indicadorMainPanel" id = "altimeter">
+    <div class="center-point" ></div>
+   <!--     
     <div clas="central-hider" id="altimeter-hide"></div>
-
+ 
     <div class="alimeter-scale hundreds-feets-1" style="--a:1"></div>
     <div class="alimeter-scale hundreds-feets-2" style="--a:2"></div>
     <div class="alimeter-scale hundreds-feets-3" style="--a:3"></div>
     <div class="alimeter-scale hundreds-feets-4" style="--a:4"></div>
     <div class="alimeter-scale hundreds-feets-5" style="--a:5"></div>
-    <div class="center-point" ></div>
     
+     
     <div class="hundred hund-0" style="--i:0"><b>0</b></div>
     <div class="hundred hund-1" style="--i:1"><b>1</b></div>
     <div class="hundred hund-2" style="--i:2"><b>2</b></div>
@@ -19,8 +21,15 @@
     <div class="hundred hund-7" style="--i:7"><b>7</b></div>
     <div class="hundred hund-8" style="--i:8"><b>8</b></div>
     <div class="hundred hund-9" style="--i:9"><b>9</b></div>
-    <div class="needle-wraper" id="nw">
-      <div class="primary-needle" id="pn"></div>
+    --> 
+    <div class="needle-wraper pn">
+      <div class="primary-needle"></div>
+    </div>
+    <div class="needle-wraper sn">
+      <div class="secondary-needle"></div>
+    </div>
+    <div class="needle-wraper tn">
+      <div class="tertiary-needle"></div>
     </div>
     
   </div>
@@ -29,24 +38,43 @@
 <script>
 import { onMounted } from 'vue';
 
-  export default {}
-  mounted:{
-    let needle = document.querySelector(".needle-wraper");
-    /*while(true){
-      let i=90;
-      needle.style.transform = `rotate(${i}deg)`;
-      i++;
-    }*/
-    const UpdateTime = ()=>{
-      //console.log("time");
-      let date = new Date(),
-      //secToDeg = (date.getSeconds()) ;
-      secToDeg =+ 36;
-      console.log(secToDeg);
-      needle.style.transform = `rotate(${secToDeg}deg)`;
+  export default {
+    data(){
+
+      return{
+        
+      }
+    },
+    mounted(){
+      
+      let primaryNeedle = document.querySelector(".pn")
+      let senconaryNeedle = document.querySelector(".sn")
+      let tertiaryNeedle = document.querySelector(".tn")
+
+      //variable que modifica la aguja primaria (centenas de pies 1 --> 100) 
+      let degPn = this.getDeegresPrimary(90)
+      primaryNeedle.style.transform = `rotate(${degPn}deg)`
+      //variable que modifica la aguja secundaria (miles de pies  1 --> 1 000)
+      let degSn = 180
+      senconaryNeedle.style.transform = `rotate(${degSn}deg)`
+      //variable que modifica la aguja terciaria (decenas de miles 1--> 10 000)
+      let degTn = 270
+      tertiaryNeedle.style.transform = `rotate(${degTn}deg)`
+    },
+    methods:{
+      getDeegresPrimary(aux){
+        return aux;
+      },
+      getDeegresSecondary(aux){
+        return aux;
+      },
+      getDeegresTerciary(aux){
+        return aux;
+      }
     }
-    //setInterval(UpdateTime,1000)
+
   }
+
   
 </script>
 
@@ -54,7 +82,11 @@ import { onMounted } from 'vue';
   #altimeter{
     border: 2px solid grey ;
     position: relative;
-    background-color: black;
+    background-image: url(https://i.ibb.co/f2TgqSJ/alt1.png);
+    background-size: cover;
+    top: 0px;
+    left: 0px;
+
   }
   .center-point{
     width: 5px;
@@ -64,9 +96,9 @@ import { onMounted } from 'vue';
     position: absolute;
     top: 33px;
     left: 33px;
-    z-index: 2;
+    z-index: 20;
   }
-  
+/*  
   .alimeter-scale{
     width: 2px;
     height: 70px;
@@ -76,7 +108,7 @@ import { onMounted } from 'vue';
     left: 34px;
     transform: rotate(calc(72deg* var(--a)))
   }
-  /*
+  
   .hundreds-feets-1{
     transform: rotate(72deg);
   }
@@ -92,7 +124,6 @@ import { onMounted } from 'vue';
   .hundreds-feets-5{
     transform: rotate(360deg);
   }
-  */
   #altimeter-hide{
     width: 60px;
     height: 60px;
@@ -119,7 +150,7 @@ import { onMounted } from 'vue';
     display: inline-block;
     transform: rotate(calc(-36deg* var(--i)));
   }
-
+*/
   .needle-wraper{
     width: 30px;
     height: 30px;
@@ -133,12 +164,30 @@ import { onMounted } from 'vue';
   }
   .primary-needle{
     width: 2px;
-    height: 30px;
+    height: 24px;
     position: absolute;
     left: 14px;
-    top: -14px;
+    top: -10px;
     background-color: whitesmoke;
-    z-index: 10;
+    z-index: 1;
+  }
+  .secondary-needle{
+    width: 3px;
+    height: 14px;
+    position: absolute;
+    left: 13px;
+    top: 1px;
+    background-color: whitesmoke;
+    z-index: 1;
+  }
+  .tertiary-needle{
+    width: 0.5px;
+    height: 25px;
+    position: absolute;
+    left: 15px;
+    top: -9px;
+    background-color: whitesmoke;
+    z-index: 1;
   }
 
 
