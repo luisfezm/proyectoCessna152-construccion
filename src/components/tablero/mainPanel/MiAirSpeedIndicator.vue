8f172210155556195bc2e66d12825c38f5dfafec
Daburuso <felizben32@hotmail.com>
2023-06-13 21:59:30 -0400

    feat: se avanza en el airspeed

--- src/components/tablero/mainPanel/MiAirSpeedIndicator.vue
+++ src/components/tablero/mainPanel/MiAirSpeedIndicator.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="indicadorMainPanel">Airspeed Indicator</div>
+  <div
+    style="position: relative; display: inline-block; text-align: center"
+    class="imagenAirspeedIndicator"
+  >
+    <img
+      id="imagenAirspeed"
+      style="height: 25%; width: 100%"
+      src="../../../assets/img/Airspeed_Indicator.png"
+    />
+    <div id="whiteBox" />
+  </div>
 </template>
 
 <script>
   export default {}
 </script>
+<style scoped>
+  #imagenAirspeedIndicator {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #whiteBox {
+    background: rgb(255, 255, 255);
+
+    position: absolute;
+    background-color: red;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 35%;
+    width: 26%;
+    padding: 2%;
+    transform: rotate(deg);
+  }
+</style>
