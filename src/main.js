13f66222c0a12dd83a70e22fccbd30b2cfb206f9
Felipe Martinez <fmartinez19@alumnos.utalca.cl>
2023-06-14 00:36:30 -0400

    feat: implementacion logica yoke
    Se implementó la lógica del yoke. Si se presiona cualquiera de las teclas W,A,S,D, el yoke cambia su estado.

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ import store from './store';
 
 createApp(App).use(store).mount('#app');
 
+window.addEventListener('keydown', event => {
+    const key = event.key.toUpperCase()
+    store.dispatch('presionarTecla_yoke', key)
+  })
+  
+  // Evento para soltar una tecla
+  window.addEventListener('keyup', () => {
+    store.dispatch('soltarTecla_yoke')
+  })
--- src/modules/interactuadores/interactuadores.js
+++ src/modules/interactuadores/interactuadores.js
@@ -1,19 +1,63 @@
 // interactuadores.js
 
 const interactuadores = {
-    state: {
-      // Estado inicial de los interactuadores
+  state: {
+    // Estado inicial de los interactuadores
+    estadoPitch_yoke: 0,
+    estadoRoll_yoke: 0
+  },
+  mutations: {
+    // Mutaciones para modificar el estado de los interactuadores
+    setEstadoPitch_yoke(state, pitch_yoke) {
+      state.estadoPitch_yoke = pitch_yoke
     },
-    mutations: {
-      // Mutaciones para modificar el estado de los interactuadores
-    },
-    actions: {
-      // Acciones para realizar operaciones relacionadas con los interactuadores
-    },
-    getters: {
-      // Getters para obtener datos del estado de los interactuadores
+    setEstadoRoll_yoke(state, roll_yoke) {
+      state.estadoRoll_yoke = roll_yoke
     }
-  };
-  
-  export default interactuadores;
-  
+  },
+  actions: {
+    // Acciones para realizar operaciones relacionadas con los interactuadores
+    presionarTecla_yoke({ commit, state }, tecla) {
+      switch (tecla) {
+        case 'W':
+          commit('setEstadoPitch_yoke', 1)
+          break
+        case 'S':
+          commit('setEstadoPitch_yoke', -1)
+          break
+        case 'A':
+          commit('setEstadoRoll_yoke', -1)
+          break
+        case 'D':
+          commit('setEstadoRoll_yoke', 1)
+          break
+      }
+
+      console.log('Nuevo estado del yoke:')
+      console.log('Pitch_yoke:', state.estadoPitch_yoke)
+      console.log('Roll_yoke:', state.estadoRoll_yoke)
+      console.log('---')
+    },
+    soltarTecla_yoke({ commit, state }) {
+      commit('setEstadoPitch_yoke', 0)
+      commit('setEstadoRoll_yoke', 0)
+      console.log('Pitch_yoke:', state.estadoPitch_yoke)
+      console.log('Roll_yoke:', state.estadoRoll_yoke)
+    }
+  },
+  getters: {
+    // Getters para obtener datos del estado de los interactuadores
+    
+    getEstadoPitch_yoke(state) {
+      return state.estadoPitch_yoke;
+    },
+    getEstadoRoll_yoke(state) {
+      return state.estadoRoll_yoke;
+    }
+   
+
+    
+  }
+};
+
+export default interactuadores;
