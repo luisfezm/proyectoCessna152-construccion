4c1009cc2693e7941bf2ddbfe76268a1a7a2f0e5
JavierVF <jvillarroel20@alumnos.utalca.cl>
2023-06-18 23:55:07 -0400

    feat: throttle
    se integró el valor (throttle) con la palanca Se creó un setter en el archivo throttle

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ let temp
 window.addEventListener('keypress', (event) => {
   if (event.key === 'c') {
     clearTimeout(timeoutThrottle)
-    store.dispatch('presionarThrottle')
+    //store.dispatch('presionarThrottle')
     console.log('throttle_depth', store.getters.getThrottleDepth)
   } else if (event.key === 'v') {
     clearTimeout(timeoutThrottle)
-    store.dispatch('tirarThrottle')
+    //store.dispatch('tirarThrottle')
     console.log('throttle_depth', store.getters.getThrottleDepth)
   }
 })
