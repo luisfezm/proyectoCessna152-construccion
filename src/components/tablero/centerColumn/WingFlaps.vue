f7fc93be5637d07dcb2c116e50f447a07e2d5115
Daburuso <felizben32@hotmail.com>
2023-06-20 17:51:17 -0400

    feat: se implementa el wingflaps y el wingflapsangle

--- src/components/tablero/centerColumn/WingFlaps.vue
+++ src/components/tablero/centerColumn/WingFlaps.vue
@@ -21,6 +21,28 @@
       {{ currentTranslationY }}
     </div>
   </div>
+  <div
+    id="miDiv"
+    class="WingFlapsAngle"
+    :class="{ grabbingAngle: isDraggingAngle }"
+    @mousedown="startDragAngle"
+    @mouseup="stopDragAngle"
+    @mousemove="handleDragAngle"
+  >
+    <img
+      ref="imageAngle"
+      src="src\assets\rectangle.png"
+      :style="{
+        transform: `translateY(${currentTranslationYAngle}px)`,
+        width: '100%',
+        height: '10%',
+      }"
+      draggable="false"
+    />
+    <div class="title">
+      {{ currentTranslationYAngle }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,13 +51,56 @@
       return {
         startX: 0,
         startY: 0,
+        startXAngle: 0,
+        startYAngle: 0,
         isDragging: false,
+        isDraggingAngle: false,
         translationMultiplier: 1, // Factor de desplazamiento vertical
         lastTranslationY: 0, // Última posición de desplazamiento vertical
         currentTranslationY: 0, // Desplazamiento vertical actual
+        translationMultiplierAngle: 1, // Factor de desplazamiento vertical
+        lastTranslationYAngle: 0, // Última posición de desplazamiento vertical
+        currentTranslationYAngle: 0, // Desplazamiento vertical actual
+        velocity: 100,
       }
     },
     methods: {
+      startDragAngle(event) {
+        event.preventDefault() // Evita el arrastre predeterminado de la imagen
+        this.startXAngle = event.clientX
+        this.startYAngle = event.clientY
+        this.isDraggingAngle = true
+      },
+      stopDragAngle() {
+        this.isDraggingAngle = false
+        this.lastTranslationYAngle = this.currentTranslationYAngle // Guardar la última posición al soltar la imagen
+      },
+      handleDragAngle(event) {
+        if (this.isDraggingAngle) {
+          const currentY = event.clientY
+          const diffY = currentY - this.startYAngle
+
+          const translationFactor = 5 // Factor de desplazamiento
+          let newTranslationY = diffY * translationFactor
+
+          const maxTranslationY = 60 // Límite máximo de desplazamiento hacia arriba
+          const minTranslationY = 0 // Límite máximo de desplazamiento hacia abajo
+
+          newTranslationY = Math.max(
+            minTranslationY,
+            Math.min(maxTranslationY, newTranslationY)
+          )
+
+          const nextTranslationY = this.lastTranslationYAngle + newTranslationY
+          this.currentTranslationYAngle = Math.max(
+            minTranslationY,
+            Math.min(maxTranslationY, nextTranslationY)
+          )
+          this.calculateVelocity() // Ajustar dentro del rango
+
+          this.$refs.imageAngle.style.transform = `translateYAngle(${this.currentTranslationYAngle}px)`
+        }
+      },
       startDrag(event) {
         event.preventDefault() // Evita el arrastre predeterminado de la imagen
         this.startX = event.clientX
@@ -51,11 +116,11 @@
           const currentY = event.clientY
           const diffY = currentY - this.startY
 
-          const translationFactor = 1 // Factor de desplazamiento
+          const translationFactor = 5 // Factor de desplazamiento
           let newTranslationY = diffY * translationFactor
 
-          const maxTranslationY = 40 // Límite máximo de desplazamiento hacia arriba
-          const minTranslationY = -40 // Límite máximo de desplazamiento hacia abajo
+          const maxTranslationY = 70 // Límite máximo de desplazamiento hacia arriba
+          const minTranslationY = 0 // Límite máximo de desplazamiento hacia abajo
 
           newTranslationY = Math.max(
             minTranslationY,
@@ -66,20 +131,84 @@
           this.currentTranslationY = Math.max(
             minTranslationY,
             Math.min(maxTranslationY, nextTranslationY)
-          ) // Ajustar dentro del rango
+          )
+          this.calculateVelocity() // Ajustar dentro del rango
 
           this.$refs.image.style.transform = `translateY(${this.currentTranslationY}px)`
         }
+      },
+      calculateVelocity() {
+        const currentVelocity = this.velocity
+        switch (true) {
+          case this.currentTranslationY == 0 &&
+            this.currentTranslationYAngle == 0:
+            this.velocity = currentVelocity
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 35 &&
+            this.currentTranslationYAngle == 0:
+            this.velocity = currentVelocity * 0.95
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 70 &&
+            this.currentTranslationYAngle == 0:
+            this.velocity = currentVelocity * 0.9
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 35 &&
+            this.currentTranslationYAngle == 20:
+            this.velocity = currentVelocity * 0.85
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 70 &&
+            this.currentTranslationYAngle == 20:
+            this.velocity = currentVelocity * 0.8
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 35 &&
+            this.currentTranslationYAngle == 40:
+            this.velocity = currentVelocity * 0.75
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 70 &&
+            this.currentTranslationYAngle == 40:
+            this.velocity = currentVelocity * 0.7
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 35 &&
+            this.currentTranslationYAngle == 60:
+            this.velocity = currentVelocity * 0.65
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+          case this.currentTranslationY == 70 &&
+            this.currentTranslationYAngle == 60:
+            this.velocity = currentVelocity * 0.6
+            console.log('La velocidad actual es ' + this.velocity)
+            break
+        }
+        this.velocity = 100
       },
     },
   }
 </script>
 
-<style>
+<style scoped>
   .WingFlaps {
     display: flex;
     justify-content: center;
-    align-items: center;
+
+    background-color: rgb(177, 177, 177);
+    width: 50%;
+    height: 80%;
+    border-radius: 5%;
+    margin: auto;
+    position: relative;
+    cursor: grab;
+  }
+  .WingFlapsAngle {
+    display: flex;
+    justify-content: center;
+
     background-color: rgb(177, 177, 177);
     width: 50%;
     height: 80%;
@@ -90,6 +219,10 @@
   }
 
   .WingFlaps.grabbing {
+    cursor: grabbing;
+  }
+
+  .WingFlapsAngle.grabbing {
     cursor: grabbing;
   }
 
