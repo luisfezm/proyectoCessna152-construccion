4dcb5ca2f48f7565bea0c5ed6af3db2c3239242e
Romina Fernández <rofernandez18@alumnos.utalca.cl>
2023-06-26 15:34:00 -0400

    fix: radio (incompleta)

--- src/components/tablero/radioPanel/MiComm1.vue
+++ src/components/tablero/radioPanel/MiComm1.vue
@@ -1,38 +1,68 @@
 <template>
   <div class="panel-radio">
-    
     <div class="controles">
       <div class="grupo-com">
-        <div :style="{ color: com1TextColor }">COM1: {{ com1 }} </div> 
-        <input type="text" v-model="com1" class="input-frecuencia">
+        <div class="label">COM1:</div>
+        <div class="input-wrapper">
+          <input type="text" v-model="com1" class="input-frecuencia" @input="handleCom1Change" />
+          <vue-slider v-model="com1" :min="108.0" :max="137.0" :step="0.025" class="slider" />
+        </div>
       </div>
       <div class="grupo-com">
-        <div :style="{ color: com1TextColor }">COM2: {{ com2 }} </div> 
-        <input type="text" v-model="com2" class="input-frecuencia">
+        <div class="label">COM2:</div>
+        <div class="input-wrapper">
+          <input type="text" v-model="com2" class="input-frecuencia" @input="handleCom2Change" />
+          <vue-slider v-model="com2" :min="108.0" :max="137.0" :step="0.025" class="slider" />
+        </div>
       </div>
       <div class="grupo-nav">
-        <div :style="{ color: com1TextColor }">NAV1: {{ nav1 }} </div> 
-        <input type="text" v-model="nav1" class="input-frecuencia">
+        <div class="label">NAV1:</div>
+        <div class="input-wrapper">
+          <input type="text" v-model="nav1" class="input-frecuencia" @input="handleNav1Change" />
+          <vue-slider v-model="nav1" :min="108.0" :max="117.95" :step="0.025" class="slider" />
+        </div>
       </div>
       <div class="grupo-nav">
-        <div :style="{ color: com1TextColor }">NAV2: {{ nav2 }} </div>
-        <input type="text" v-model="nav2" class="input-frecuencia">
+        <div class="label">NAV2:</div>
+        <div class="input-wrapper">
+          <input type="text" v-model="nav2" class="input-frecuencia" @input="handleNav2Change" />
+          <vue-slider v-model="nav2" :min="108.0" :max="117.95" :step="0.025" class="slider" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import VueSlider from 'vue-slider-component';
+  import 'vue-slider-component/theme/default.css';
+
   export default {
+    components: {
+      VueSlider,
+    },
     data() {
       return {
-        com1: '118.1',
-        com2: '123.4',
-        nav1: '108.8',
-        nav2: '115.0',
-        com1TextColor: 'orange',
+        com1: 118.1,
+        com2: 123.4,
+        nav1: 108.8,
+        nav2: 115.0,
       };
-    }
+    },
+    methods: {
+      handleCom1Change(value) {
+        this.com1 = parseFloat(value);
+      },
+      handleCom2Change(value) {
+        this.com2 = parseFloat(value);
+      },
+      handleNav1Change(value) {
+        this.nav1 = parseFloat(value);
+      },
+      handleNav2Change(value) {
+        this.nav2 = parseFloat(value);
+      },
+    },
   };
 </script>
 
@@ -56,18 +86,28 @@
 .grupo-nav {
   display: flex;
   align-items: center;
+  color: white;
 }
 
 .label {
-  width: 50px;
+  width: 60px;
   font-weight: bold;
-  color: white;
+}
+
+.input-wrapper {
+  display: flex;
+  align-items: center;
 }
 
 .input-frecuencia {
   padding: 5px;
   width: 80px;
-  border: 1px solid #ccc;
+  border: 1px solid #f48549;
   border-radius: 4px;
+}
+
+.slider {
+  margin-left: 10px;
+  width: 180px
 }
 </style>
