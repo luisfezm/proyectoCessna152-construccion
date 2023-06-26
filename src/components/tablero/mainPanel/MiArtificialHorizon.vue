9d0fdc1b64059024b3f091eac66641e452daffca
NBJ <nbarahona19@alumnos.utalca.cl>
2023-06-26 19:15:50 -0400

    Fix:ArtificialHorizon

--- src/components/tablero/mainPanel/MiArtificialHorizon.vue
+++ src/components/tablero/mainPanel/MiArtificialHorizon.vue
@@ -1,67 +1,32 @@
 <template>
   <div class="indicadorMainPanel">
-    <Attitude id="attitude" :size="70" :pitch="pitchValue"  :roll="rollValue" :class="{ 'rotate-animation': isAnimating }" />
+    <Attitude id="attitude" :size="70" :pitch="0"  :roll="0"  />
   </div>
 </template>
 
 <script>
   import { Attitude } from 'vue-flight-indicators'
-  import { mapGetters } from 'vuex'
+
 
   export default {
     name: 'IndicadorMainPanel',
     components: {
       Attitude,
     },
-    data() {
-      return {
-        pitchValue: 0, // Valor inicial de pitch
-        rollValue: 0,  // Valor inicial de roll
-        isAnimating: false, // Nuevo dato para controlar la animación
-      };
-    },
-    methods: {
-  cambiarValores(pitch, roll) {
-    this.isAnimating = true;
-    
-    setTimeout(() => {
-      this.pitchValue = pitch;
-      this.rollValue = roll;
-      this.isAnimating = false;
-      
-      console.log("-------------------------")
-      console.log("RollVUE = " + this.rollValue)
-      console.log("Roll = " + this.getRoll())
-      console.log("PitchVUE = " + this.pitchValue)
-      console.log("Pitch = " + this.getPitch())
-      console.log("-------------------------")
-    }, 2000);
-  },
-      ...mapGetters(['getCurrentTranslationY']),
-      pitch() {
-        return this.getCurrentTranslationY * 0.4 // Fórmula para que el movimiento del pitch sea más suave
-      },
-      // Obtener el pitch y roll desde HorizonteArtificial.js
-      getPitch(){ 
-        return this.$store.getters.anguloPitch;
-      },
-      getRoll(){
-        return this.$store.getters.anguloRoll;
-      }
-    },
+
+
     mounted() {
   setInterval(() => {
-    const pitch = this.getPitch();
-    const roll = this.getRoll();
-    
-    this.cambiarValores(pitch, roll);
-    
+ 
+
     const attitudeQ = document.querySelector(".roll.indicator-box");
-    attitudeQ.style.cssText = `transform: rotate(${this.pitchValue}deg); transition: 1s;`
+    attitudeQ.style.cssText = `transform: rotate(${this.$store.getters.anguloRoll}deg); transition: 1s;`;
     
   
-    const inAttitude = document.querySelector(".pitch.indicator-box");
-    inAttitude.style.cssText = `transition: 1s;`
+    const inAttitude = (this.$store.getters.anguloPitch)/5;
+    const element = document.querySelector('.pitch.indicator-box');
+    element.style.cssText = `transform: translateY(${inAttitude}px); transition: 1s`;
+
   }, 500);
 },
 
--- src/modules/indicadores/horizonteArtificial.js
+++ src/modules/indicadores/horizonteArtificial.js
@@ -1,5 +1,4 @@
 // horizonteArtificial.js
-// bajo los comentarios de (FALTA AJUSTAR) hay valores que pueden variar para modificar el comportamiento del horizonte artificial
 
 const horizonteArtificial = {
   state: {
@@ -8,160 +7,87 @@ const horizonteArtificial = {
     anguloPitch: 0,
     velocidadRoll: 0,
     velocidadPitch: 0,
-    // (FALTA AJUSTAR)
     maxVelocidadRoll: 100,
     maxVelocidadPitch: 100,
   },
   mutations: {
-    // Mutaciones para modificar el estado de los indicadores
     setmaxVelocidadRoll(state, max) {
-      state.maxVelocidadRoll = max
+      state.maxVelocidadRoll = max;
     },
 
     setMaxVelocidadPitch(state, max) {
-      state.maxVelocidadPitch = max
+      state.maxVelocidadPitch = max;
     },
 
     actualizaAngulos(state) {
-      // (FALTA AJUSTAR)
-      // varia los angulos segun la velocidad
-
-      //Originalmente era state.anguloRoll += state.velocidadRoll / 20, se cambio a 2 para que avanzara mas grados y sea notoria la rotación
-      state.anguloRoll += state.velocidadRoll / 2
-      state.anguloPitch += state.velocidadPitch / 2
-
-      // reinicia los angulos de 360 a 0
-      state.anguloRoll = state.anguloRoll % 360
-      state.anguloPitch = state.anguloPitch % 360
-
-      console.log('ACTUALIZAR: ' + state.anguloRoll)
+      // Incrementar los ángulos de roll y pitch según las velocidades
+      state.anguloRoll += state.velocidadRoll ;
+      state.anguloPitch += state.velocidadPitch;
+    
+      // Controlar las rotaciones completas (de 0 a 360 grados)
+      state.anguloRoll = state.anguloRoll % 360;
+      state.anguloPitch = state.anguloPitch % 360;
+    
+      console.log('ACTUALIZAR: ' + state.anguloRoll);
     },
 
     actualizar(state, Yoke) {
-      // saco los componentes del Yoke
-      let roll = Yoke.roll
-      let pitch = Yoke.pitch
-      // (FALTA AJUSTAR)
-      let disminucion = 1 // este es valor por el cual disminuyen las velocidades
+      let roll = Yoke.roll;
+      let pitch = Yoke.pitch;
 
-      // tiendo a 0 las velocidades
-      if (state.velocidadRoll != 0) {
-        if (state.velocidadRoll > 0) {
-          state.velocidadRoll -= disminucion
-          if (state.velocidadRoll < 0) {
-            // si me paso lo seteo en 0
-            state.velocidadRoll = 0
-          }
-        } else {
-          state.velocidadRoll += disminucion
-          if (state.velocidadRoll > 0) {
-            // si me paso lo seteo en 0
-            state.velocidadRoll = 0
-          }
-        }
+      state.velocidadRoll = roll;
+      state.velocidadPitch = pitch;
+
+      if (roll > state.maxVelocidadRoll) {
+        state.velocidadRoll = state.maxVelocidadRoll;
+      } else if (roll < -state.maxVelocidadRoll) {
+        state.velocidadRoll = -state.maxVelocidadRoll;
       }
 
-      if (state.velocidadPitch != 0) {
-        if (state.velocidadPitch > 0) {
-          state.velocidadPitch -= disminucion
-          if (state.velocidadPitch < 0) {
-            // si me paso lo seteo en 0
-            state.velocidadPitch = 0
-          }
-        } else {
-          state.velocidadPitch += disminucion
-          if (state.velocidadPitch > 0) {
-            // si me paso lo seteo en 0
-            state.velocidadPitch = 0
-          }
-        }
-      }
-
-      if (roll != 0) {
-        // si hay roll lo uso
-        if (roll > 0) {
-          if (state.velocidadRoll < state.maxVelocidadRoll) {
-            // si aun no alcanzo la velocidad maxima
-            state.velocidadRoll += roll
-            if (state.velocidadRoll > state.maxVelocidadRoll) {
-              // si supero la velocidad maxima lo seteo en el maximo
-              state.velocidadRoll = state.maxVelocidadRoll
-            }
-          }
-        } else {
-          // caso roll en sentido contrario
-          if (-state.velocidadRoll < state.maxVelocidadRoll) {
-            state.velocidadRoll += roll
-            if (-state.velocidadRoll > state.maxVelocidadRoll) {
-              state.velocidadRoll = -state.maxVelocidadRoll
-            }
-          }
-        }
-      }
-
-      if (pitch != 0) {
-        // si hay pitch lo uso
-        if (pitch > 0) {
-          if (state.velocidadPitch < state.maxVelocidadPitch) {
-            // si aun no alcanzo la velocidad maxima
-            state.velocidadPitch += pitch
-            if (state.velocidadPitch > state.maxVelocidadPitch) {
-              // si supero la velocidad maxima lo seteo en el maximo
-              state.velocidadPitch = state.maxVelocidadPitch
-            }
-          }
-        } else {
-          // caso pitch en sentido contrario
-          if (-state.velocidadPitch < state.maxVelocidadPitch) {
-            state.velocidadPitch += pitch
-            if (-state.velocidadPitch > state.maxVelocidadPitch) {
-              state.velocidadPitch = -state.maxVelocidadPitch
-            }
-          }
-        }
+      if (pitch > state.maxVelocidadPitch) {
+        state.velocidadPitch = state.maxVelocidadPitch;
+      } else if (pitch < -state.maxVelocidadPitch) {
+        state.velocidadPitch = -state.maxVelocidadPitch;
       }
     },
   },
   actions: {
-    // Acciones para realizar operaciones relacionadas con los indicadores
-    // (FALTA AJUSTAR)
     actualizar({ commit }, Yoke) {
-      console.log(Yoke)
+      console.log(Yoke);
+
       if (Yoke.roll >= 0) {
-        // saco raices para disminuir la brecha entre los valores maximos y minimos (dependera de como se implemente el Yoke)
-        
-        Yoke.roll = Math.sqrt(Math.sqrt(Yoke.roll))
+        Yoke.roll = Math.sqrt(Math.sqrt(Yoke.roll));
       } else {
-        Yoke.roll = -Math.sqrt(Math.sqrt(-Yoke.roll))
+        Yoke.roll = -Math.sqrt(Math.sqrt(-Yoke.roll));
       }
 
       if (Yoke.pitch >= 0) {
-        Yoke.pitch = Math.sqrt(Math.sqrt(Yoke.pitch))
+        Yoke.pitch = Math.sqrt(Math.sqrt(Yoke.pitch));
       } else {
-        Yoke.pitch = -Math.sqrt(Math.sqrt(-Yoke.pitch))
+        Yoke.pitch = -Math.sqrt(Math.sqrt(-Yoke.pitch));
       }
-      commit('actualizar', Yoke)<
-      commit('actualizaAngulos')
+
+      commit('actualizar', Yoke);
+      commit('actualizaAngulos');
     },
   },
   getters: {
-    // Getters para obtener datos
     anguloRoll(state) {
-      return parseInt(state.anguloRoll)
+      return parseInt(state.anguloRoll);
     },
 
     anguloPitch(state) {
-      return parseInt(state.anguloPitch)
+      return parseInt(state.anguloPitch);
     },
 
     velocidadRoll(state) {
-      return parseInt(state.velocidadRoll)
+      return parseInt(state.velocidadRoll);
     },
 
     velocidadPitch(state) {
-      return parseInt(state.velocidadPitch)
+      return parseInt(state.velocidadPitch);
     },
   },
-}
+};
 
-export default horizonteArtificial
+export default horizonteArtificial;
