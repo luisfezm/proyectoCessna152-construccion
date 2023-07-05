c47aa075078b3cdfc482cb1d878127698c87def0
MongrelDog <mushashourrea@gmail.com>
2023-07-05 18:47:58 -0400

    fix: heading indicator

--- src/components/tablero/mainPanel/MiHeadingIndicator.vue
+++ src/components/tablero/mainPanel/MiHeadingIndicator.vue
@@ -12,35 +12,11 @@
     components: {
       Heading,
     },
-    data() {
-      return {
-        headingValue: 0, //grados de desviacion que se obtienen como parametro
-      }
-    },
-    methods: {
-      moverAvion(estado) {
-        if (estado == -1) {
-          //El avion esta girando hacia la izquierda
-          this.$store.dispatch('actualizar', { roll: -100, pitch: 0 })
-          this.headingValue = this.$store.getters.getHeadingIndicator
-          console.log('DESVIACION   ' + this.$store.getters.getHeadingIndicator)
-        }
-
-        if (estado == 1) {
-          //El avion esta girando hacia la derecha
-
-          this.$store.dispatch('actualizar', { roll: 100, pitch: 0 })
-          this.headingValue = this.$store.getters.getHeadingIndicator
-
-          console.log('DESVIACION   ' + this.$store.getters.getHeadingIndicator)
-        }
-        if (this.headingValue >= 25) {
-          this.headingValue = 25
-        }
-
-        if (this.headingValue <= -25) {
-          this.headingValue = -25
-        }
+    computed: {
+      headingValue: {
+        get: function () {
+          return this.$store.state.headingValue
+        },
       },
     },
   }
