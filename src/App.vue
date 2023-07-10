125f28d85cf46f62edc32a38aec1cc0de6ddd46b
Matias Camilla <matias.camilla.dog@gmail.com>
2023-07-10 15:32:23 -0400

    feat: calculo de altura según potencia (falta store para angulo)
    se dejo asi para hacer pruebas ya que todavia esta muerto el yoke

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@
         V: 0,
         angulo_avion: 0,
 
-        headAvion: 10,
+        headAvion: 9,
 
         //quisas en el futuro meterlo al avion
         coordenadas_actuales: {
@@ -103,6 +103,9 @@
         console.log('poentecia:' + this.potencia)
         this.calcularVelocidadDespuesDeRotacion(this.V, this.angulo_avion)
 
+        //angulo pich
+        //store.getters.anguloroll
+
         this.calcularVelocidadAltura(this.V, this.headAvion)
 
         console.log('velX:', store.getters.velocidad_x)
@@ -115,6 +118,11 @@
         // actualizar posicion
         this.coordenadas_actuales.latitud += store.getters.velocidad_y * 0.1
         this.coordenadas_actuales.longitud += store.getters.velocidad_x * 0.1
+
+        store.dispatch(
+          'setAltura',
+          store.getters.altura + store.getters.velocidad_z * 0.1
+        )
 
         store.dispatch('setCoordenadas', this.coordenadas_actuales)
         console.log(
@@ -137,9 +145,7 @@
 
       calcularVelocidadAltura(velocidad, anguloVertical) {
         var anguloRadianes = (anguloVertical * Math.PI) / 180
-        var velocidadaltura = velocidad * Math.cos(anguloRadianes)
-
-        console.log('velocidadaltura', velocidadaltura)
+        var velocidadaltura = velocidad * Math.sin(anguloRadianes)
 
         store.dispatch('setVelocidadZ', velocidadaltura)
       },
@@ -150,11 +156,8 @@
 
       //usar la funcion anterior modificando de forma estruccturada el store para simular cosas ()
 
-      //mover el punto del mapa xd  [tavo]
-      //asignar posiscion inicial del avion
-      //aplicar funcion ya creada
-
       //trabajar el div para mostrar la vista [opcional]
+      //modificar los divs para que no queden espacios
 
       //crear .vue para visualizar la primera persona
 
